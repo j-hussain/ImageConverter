0b4a6a75a8d37a10756b02900a9804a6a61ea464
Jabir Hussain <jabir-hussain@hotmail.co.uk>
2021-08-09 14:52:43 +0100

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,15 @@
+import argparse
+from PIL import Image
+
+parser = argparse.ArgumentParser(description="Simple CLI image converter")
+parser.add_argument("--file", type=str, help="Required arg: filename")
+parser.add_argument("--type", type=str, help="A required argument: state which file type you want to convert it to")
+
+# parse the arguments
+args = parser.parse_args()
+old_image = Image.open(args.file)
+if not old_image.mode == "RGB":
+    # convert to RGB
+    old_image = old_image.convert("RGB")
+
+old_image.save(args.file[:len(file)-3] + type)
